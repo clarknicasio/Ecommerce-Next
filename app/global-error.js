b87eabb4974e408552ef610cae7e32601ef7f6be
nicasioclark <nicasioclark@myfloradna.com>
2024-09-08 19:58:13 -0300

    Styling global error

--- app/global-error.js
+++ app/global-error.js
@@ -4,9 +4,9 @@ export default function GlobalError({ error, reset }) {
 
     return ( 
     <html>
-        <body>
-            <h2>Oops ... algo no salió bien</h2>
-            <button onClick={() => reset()}>Recargar</button>
+        <body className="h-screen flex flex-col justify-center items-center text-center">
+            <h2 className="font-bold mb-4">Oops ... algo no salió bien</h2>
+            <button className="bg-green-500 text-white py-2 px-4 rounded" onClick={() => reset()}>Recargar</button>
         </body>
     </html>
     )
