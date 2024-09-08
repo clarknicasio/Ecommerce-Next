4327422329403dbff12a26e330a0aab634281417
nicasioclark <nicasioclark@myfloradna.com>
2024-09-08 12:03:22 -0300

    Deployment fixes

--- app/admin/layout.js
+++ app/admin/layout.js
@@ -1,16 +1,15 @@
 "use client";
 
 import { useAuthContext } from "../context/authContext";
-import LoginPage from "./login/LoginPage"; // quitar y usar el paralel route en carpeta login con el @ y cambiar LoginPage.js por page.js
+import LoginPage from "./login/LoginPage"; 
 
-const AdminLayout = ({children, login}) => { 
+const AdminLayout = ({children}) => { 
 
     const {user} = useAuthContext();
 
     return (
         <>
             { 
-            //user.logged ? children : login 
             user?.logged ? children : <LoginPage/> 
             }
         </>
